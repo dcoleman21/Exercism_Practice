4ecebd494db78ca6f0cf34c9b21a0cd396cdd073
Dani Coleman <danicoleman00@gmail.com>
2021-03-19 10:30:33 -0500

    Complete working method

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -8,20 +8,25 @@ To get started with TDD, see the `README.md` file in your
 
 class Luhn
   def self.valid?(nums)
-    no_space_nums = nums.gsub(/\s+/, "").chars.reverse
-    if no_space_nums.length <= 1
-      false
-    else
-      doubled_nums = no_space_nums.map.with_index do |num, index|
-        if index%2 == 1
-          num.to_i * 2
-        else
-          num.to_i
-        end
-      end
-      doubled_nums
-      # need to subract 9 from any number that is greater than 9
-      require "pry"; binding.pry
+    no_space_nums = nums.delete(' ')
+    if no_space_nums.scan(/[A-Za-z\W]/) != [] || no_space_nums.length <= 1
+      return false
     end
+    # need to double the second digits
+    doubled_nums = no_space_nums.chars.reverse.map.with_index do |num, index|
+      if index%2 == 1
+        num = num.to_i * 2
+        if num > 9
+          num - 9
+        else
+          num
+        end
+      else
+        num.to_i
+      end
+    # need to sum the digits
+    end.sum
+    # check if sum is divisible by 10
+    return doubled_nums % 10 == 0
   end
 end
