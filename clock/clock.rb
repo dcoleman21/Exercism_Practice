531bbea3c232235b873164d8febe040487c0c096
Dani Coleman <danicoleman00@gmail.com>
2021-03-25 09:50:36 -0500

    Complete all methods to pass all tests
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- clock/clock.rb
+++ clock/clock.rb
@@ -8,14 +8,27 @@ To get started with TDD, see the `README.md` file in your
 
 class Clock
   attr_reader :hour,
-              :min
+              :minute
 
-  def initialize(hour, min={minute: 0})
-    @hour = hour[:hour]
-    @min = min[:minute]
+  def initialize(hour: 0, minute: 0)
+    @hour = (hour + minute / 60) % 24
+    @minute = minute % 60
   end
 
   def to_s
-    "#{@hour.to_s.rjust(2, '0')}:#{@min.to_s.rjust(2, '0')}"
+    sprintf("%02d:%02d", hour, minute) # how does it know where to separate hours and minutes @ the colon?
+    # "#{@hour.to_s.rjust(2, '0')}:#{@minute.to_s.rjust(2, '0')}"
+  end
+
+  def +(clock) # how does ruby know to use this as both a method and for addition
+    Clock.new(hour: (clock.hour + @hour), minute: (clock.minute + @minute))
+  end
+
+  def -(clock) # how does ruby know to use this as both a method and for subtraction
+    Clock.new(hour: (@hour - clock.hour), minute: (@minute - clock.minute))
+  end
+
+  def ==(clock) # how does ruby know to use this as both a method and for equals
+    @hour == clock.hour && @minute == clock.minute
   end
 end
