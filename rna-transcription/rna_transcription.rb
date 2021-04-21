b5c4f5727945a7aebf9439417809ce2b19deed44
callbritton <cla.inbox@gmail.com>
2021-04-21 11:11:53 -0400

    Complete rna_transcription exercise!
    Co-authored-by: Dani Coleman <danicoleman00@gmail.com>

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -6,3 +6,32 @@ To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
 
+class Complement
+  RNA_CODE = {
+    'G' => 'C',
+    'C' => 'G',
+    'A' => 'U', 
+    'T' => 'A'
+  }
+
+  def self.of_dna(dna_strand)
+  #   if dna_strand == 'C'
+  #     'G'
+  #   elsif dna_strand == 'G'
+  #     'C'
+  #   elsif dna_strand == 'A'
+  #     'U'
+  #   elsif dna_strand == 'T'
+  #     'A'
+  #   else 
+  #     dna_strand
+  #   end
+  # end
+    strand_array = dna_strand.split('')
+    
+    strand_array.each_with_index do  |base, index|
+      strand_array[index] = RNA_CODE[base]
+    end
+    strand_array.join
+  end
+end
