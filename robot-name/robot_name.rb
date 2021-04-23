236a21fa1441917f07ceced4a076af903f7e0901
Dani Coleman <danicoleman00@gmail.com>
2021-04-23 11:08:50 -0500

    Create Robot class with forget, name and reset methods
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -5,4 +5,16 @@ Write your code for the 'Robot Name' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/robot-name` directory.
 =end
+class Robot
+  def self.forget
+    @@available_names = [*'AA000'..'ZZ999'].shuffle
+  end
 
+  def name
+    @name ||= @@available_names.pop
+  end
+
+  def reset
+    @name = nil
+  end
+end
